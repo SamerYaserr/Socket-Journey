f35af0cd971987409d51078f840109dd3bc1e8d1
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-08-29 16:38:53 +0300

    Integrating Socket.IO

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import { join } from "node:path";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 const app = express();
 
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
 app.get("/", (req, res) => {
-  res.send("Welcome!");
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-export default app;
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+export default httpServer;
