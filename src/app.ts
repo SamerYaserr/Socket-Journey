252ee6886546c48c42ecaedbef15bbeb1b0597f5
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-08-29 17:34:20 +0300

    Simple chat application

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,37 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
+/**
+ * emit: publish an event usiing emit('eventName', data)
+ *
+ * on: listen for an event using on('eventName', callback)
+ */
+
+/**
+ * io: server-side socket.io instance
+ *
+ * socket: client-side socket.io instance
+ */
+
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  // when a user connects
+  socket.on("chat message", (msg) => {
+    // when the server receives a 'chat message' event
+
+    console.log("message: " + msg);
+
+    io.emit("chat message", msg);
+    // emit the message to all connected clients
+  });
+
+  socket.on("typing", () => {
+    // broadcast: send the event to all clients except the one that triggered the event
+    socket.broadcast.emit("show_typing");
+  });
+
+  socket.on("stop_typing", () => {
+    socket.broadcast.emit("hide_typing");
+  });
 });
 
 export default httpServer;
